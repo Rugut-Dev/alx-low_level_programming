dd68018695083775b92cb20b02623ce137d1a903
Kelvin Rugut <krugut36@gmail.com>
2023-07-17 05:40:05 -0700

    Unix char

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1
+ */
+int main(void)
+{
+	char *message;
+
+	message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, message, 59);
+	return (1);
+}

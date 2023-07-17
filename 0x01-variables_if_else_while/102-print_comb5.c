87eb5de6607ea5f697a1c71de1f8682391921dbb
Kelvin Rugut <krugut36@gmail.com>
2023-07-17 06:43:06 -0700

    Two two-digit numbers combinations

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 99; a++)
+	{
+		for (b = a; b <= 99; b++)
+		{
+			int a_1 = a / 10;
+			int a_2 = a % 10;
+			int b_1 = b / 10;
+			int b_2 = b % 10;
+
+			putchar('0' + a_1);
+			putchar('0' + a_2);
+			putchar(' ');
+			putchar('0' + b_1);
+			putchar('0' + b_2);
+
+			if (!(a == 99 && b == 99))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}

85af0daae082c93aa666e9725b0516fd04a76857
Kelvin Rugut <krugut36@gmail.com>
2023-07-19 06:17:36 -0700

    Ever Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - finds and prints sum of even valued terms
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+	unsigned long int b, c, n, sum;
+
+	b = 1;
+	c = 2;
+	sum = 0;
+
+	for (a = 1; a <= 33; ++1)
+	{
+		if (b < 4000000 && (b % 2) == 0)
+		{
+			sum = sum + b;
+		}
+		n = b + c;
+		b = c;
+		c = n;
+	}
+
+	printf("%lu\n", sum);
+
+	return (0);
+}

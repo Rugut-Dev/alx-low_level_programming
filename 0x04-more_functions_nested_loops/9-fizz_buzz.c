5cd2935b3f02c577052c5141088599933dbeb7be
Kelvin Rugut <krugut36@gmail.com>
2023-07-20 13:08:04 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,20 +13,17 @@ int main(void)
 
 	for (n = 1; n <= 100; n++)
 	{
-		if (((n % 3) == 0) && ((n % 5) != 0))
+		if (n % 3 == 0 && n % 5 != 0)
 		{
-			printf("Fizz");
-			printf(" ");
+			printf("Fizz ");
 		}
-		else if (((n % 5) == 0) && ((n % 3) != 0))
+		else if (n % 5 == 0 && n % 3 != 0)
 		{
-			printf("Buzz");
-			printf(" ");
+			printf("Buzz ");
 		}
-		else if (((n % 5) == 0) && ((n % 3) == 0))
+		else if (n % 5 == 0 && n % 3 == 0)
 		{
-			printf("FizzBuzz");
-			printf(" ");
+			printf("FizzBuzz ");
 		}
 		else if (n == 1)
 		{
@@ -34,8 +31,7 @@ int main(void)
 		}
 		else
 		{
-			printf("%d", n);
-			printf(" ");
+			printf("%d ", n);
 		}
 	}
 	printf("\n");

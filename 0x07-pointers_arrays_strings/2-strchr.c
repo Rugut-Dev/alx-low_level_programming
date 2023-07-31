9a8d5564f6759fb2712a06954cd56b62c8596739
Kelvin Rugut <krugut36@gmail.com>
2023-07-31 04:18:16 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character ina a string
+ *
+ * @s: pointer to string;
+ * @c: character being located
+ * Return: returns pointer to first occurence or NULL if
+ * not available
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return ((char *)s);
+		}
+		s++;
+	}
+
+	return (NULL);
+}

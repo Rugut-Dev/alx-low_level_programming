c5cf7214ddfc0815011ebc5ec31782c31e816adf
Kelvin Rugut <krugut36@gmail.com>
2023-07-31 04:21:19 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#define NULL ((void *)0)
 
 /**
  * _strchr - locates a character ina a string
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-#define NULL ((void *)0)
 
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);

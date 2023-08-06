d48b513389969e59fa29a8b91c1c260ece0f4749
Kelvin Rugut <krugut36@gmail.com>
2023-08-06 09:16:36 -0700

    Print Program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - prints program's name
+ *
+ * @argc: arguments count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}

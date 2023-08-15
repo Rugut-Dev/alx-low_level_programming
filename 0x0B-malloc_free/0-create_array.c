3183fe22a94a0616c13fe8bcfe5abecf44a31e64
Kelvin Rugut <krugut36@gmail.com>
2023-08-15 03:55:05 -0700

    loat like a butterfly, sting like a bee

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_array  - creates an array of chars
+ * @size: size of the array
+ * @c: storage
+ *
+ * Return: pointer of an array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ch;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	ch = malloc(sizeof(c) * size);
+
+	if (ch == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		ch[i] = c;
+	}
+
+	return (ch);
+}

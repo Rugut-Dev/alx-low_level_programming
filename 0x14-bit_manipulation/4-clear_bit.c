b8997123f3df32965b65816b11c48413f78695b8
Kelvin Rugut <krugut36@gmail.com>
2023-09-28 15:21:52 -0700

    Task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * clar_bit - sets val of bit to 0 at a given index
+ * @n: num
+ * @index: index
+ *
+ * Return :1 if succes else 0
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int num_bits = sizeof(n) * 8;
+	unsigned long int mask = 1UL << index;
+
+	if (index >= num_bits)
+		return (-1);
+
+	*n &= ~mask;
+
+	return (1);
+}

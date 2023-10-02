51943bf244e1686f70a99b271efcfe43e484fdc9
Kelvin Rugut <krugut36@gmail.com>
2023-10-02 02:50:39 -0700

    Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,12 +12,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buffer;
 	ssize_t fd, letters_rd, letters_wr;
 
-	if (open(filename, O_RDONLY) == -1)
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
 	{
 		return (0);
 	}
 
-	fd = open(filename, O_RDONLY);
 	buffer = malloc(sizeof(char) * letters);
 	letters_rd = read(fd, buffer, letters);
 	letters_wr = write(STDOUT_FILENO, buffer, letters_rd);

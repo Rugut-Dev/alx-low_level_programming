24e8f3985a30886c3e27a744fd3b247a448bf3d6
Kelvin Rugut <krugut36@gmail.com>
2023-10-02 04:44:41 -0700

    Task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,7 +9,8 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	ssize_t fd, text_wr;
+	int fd;
+	ssize_t text_wr;
 
 	if (filename == NULL)
 		return (-1);
@@ -29,6 +30,7 @@ int append_text_to_file(const char *filename, char *text_content)
 			return (-1);
 		}
 	}
+
 	close(fd);
-	return (-1);
+	return (1);
 }

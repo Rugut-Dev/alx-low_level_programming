436e055f7c94d55ff8867431dea23ca330539a4f
Kelvin Rugut <krugut36@gmail.com>
2023-11-30 17:25:06 +0300

    print hash table'

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,5 +1,11 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table
+ *
+ * Return: void
+ */
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;

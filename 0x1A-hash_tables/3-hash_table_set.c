b6b38ba7c0167fdfe6cdf9f8a7ba7adf728797b3
Kelvin Rugut <krugut36@gmail.com>
2023-11-30 19:08:07 +0300

    Adding elem to hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,38 +11,43 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int i;
-	hash_node_t *new_node;
+	hash_node_t *new_node, *temp;
 
 	if (ht == NULL || key == NULL || strlen(key) == 0)
 	{
 		return (0);
 	}
-
 	i = key_index((const unsigned char *)key, ht->size);
-
+	temp = ht->array[i];
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			free(temp->value);
+			temp->value = strdup(value);
+			if (temp->value == NULL)
+				return (0);
+			return (1);
+		}
+		temp = temp->next;
+	}
 	new_node = malloc(sizeof(hash_node_t));
-
 	if (new_node == NULL)
 		return (0);
-
 	new_node->key = strdup(key);
 	if (new_node->key == NULL)
 	{
 		free(new_node);
 		return (0);
 	}
-
 	new_node->value = strdup(value);
-
 	if (value != NULL && new_node->value == NULL)
 	{
 		free(new_node->key);
 		free(new_node);
 		return (0);
 	}
-
 	new_node->next = ht->array[i];
 	ht->array[i] = new_node;
-
 	return (1);
 }

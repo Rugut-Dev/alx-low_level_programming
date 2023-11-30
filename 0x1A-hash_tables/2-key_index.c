92f18895e31caac11afddac3724bd298c4a37d7a
Kelvin Rugut <krugut36@gmail.com>
2023-11-30 18:07:09 +0300

    get key

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,11 +1,14 @@
 #include "hash_tables.h"
 
 /**
- * key_index - determines the index at which the key value pair should be stored
+ * key_index - determines the index at which the key/val pair should be stored
  * @key: key for index
  * @size: size of the array
+ *
+ * Return: index
  */
-unsigned long int key_index(const unsigned char *key, unsigned long int size)
+unsigned long int key_index(const unsigned char *key,
+			    unsigned long int size)
 {
 	unsigned long int hash_val, i;
 

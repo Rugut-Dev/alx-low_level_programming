a07cf0c69069583afa05b9af1ee67f3eb223154a
Kelvin Rugut <krugut36@gmail.com>
2023-11-30 16:14:44 +0300

    Adds element to the hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,42 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set = adds element to the hash table
+ * @ht: hash table
+ * @key:key
+ * @value: value
+ *
+ * Return: 1 if success and 0 if otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int i;
+	hash_node_t *new_node;
+
+	if (ht == NULL || key == NULL || strlen(key) == 0)
+	{
+		return (0);
+	}
+
+	i = key_index((const unsigned char *)key, ht->size);
+
+	new_node = malloc(sizeof(hash_node_t));
+
+	if (new_node == NULL)
+		return (0);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+
+	if (value != NULL && new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
+
+	new_node->next = ht->array[i];
+	ht->array[i] = new_node;
+
+	return (1);
+}
